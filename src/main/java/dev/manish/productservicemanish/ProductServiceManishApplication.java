ccfae29184019fe1557c079f167f7e1fd1e9e7f1
Manish Kumar <k.manu00005@gmail.com>
2024-05-16 16:29:18 +0530

    first commit

---
+++ src/main/java/dev/manish/productservicemanish/ProductServiceManishApplication.java
@@ -0,0 +1,13 @@
+package dev.manish.productservicemanish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceManishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceManishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/manish/productservicemanish/ProductServiceManishApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.manish.productservicemanish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceManishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
