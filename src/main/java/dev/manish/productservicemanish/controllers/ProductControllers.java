27df926e43e41cd7720957c157811d5a3e202263
Manish Kumar <k.manu00005@gmail.com>
2024-05-23 08:56:03 +0530

    creating service api for singleProduct and sending header and status along from controller

--- src/main/java/dev/manish/productservicemanish/controllers/ProductControllers.java
+++ src/main/java/dev/manish/productservicemanish/controllers/ProductControllers.java
@@ -2,8 +2,16 @@ package dev.manish.productservicemanish.controllers;
 
 import dev.manish.productservicemanish.dto.GetSingleProductResponseDto;
 import dev.manish.productservicemanish.dto.ProductDto;
+import dev.manish.productservicemanish.models.Product;
 import dev.manish.productservicemanish.services.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/products")
@@ -20,12 +28,35 @@ public class ProductControllers {
         return "Geting All Product";
     }
 
+//    @GetMapping("/{productId}")
+//    public Product getSingleProduct(@PathVariable("productId") Long productId) {
+////        GetSingleProductResponseDto responseDto = new GetSingleProductResponseDto();
+////        responseDto.setProduct(
+//           return productService.getSingleProduct(productId);
+//        //return "Geting Single Product : " + productId;
+//        //return responseDto;
+//    }
+
     @GetMapping("/{productId}")
-    public GetSingleProductResponseDto getSingleProduct(@PathVariable("productId") Long productId) {
-        GetSingleProductResponseDto responseDto = new GetSingleProductResponseDto();
-        responseDto.setProduct(productService.getSingleProduct(productId));
+    public ResponseEntity<Product> getSingleProduct(@PathVariable("productId") Long productId) {
+//        GetSingleProductResponseDto responseDto = new GetSingleProductResponseDto();
+//        responseDto.setProduct(
+        //Map<String,String> headers = new HashMap<>();
+        MultiValueMap<String, String> headers = new LinkedMultiValueMap<String,String>();
+        headers.add("auth-token","noaccess4uheyhey");
+
+        //along with body we are also sending header and status in response
+        ResponseEntity<Product> response = new ResponseEntity(
+                productService.getSingleProduct(productId), headers,
+                HttpStatus.NOT_FOUND);
+
+
+
+
+        return response;
+        //return productService.getSingleProduct(productId);
         //return "Geting Single Product : " + productId;
-        return responseDto;
+        //return responseDto;
     }
 
     @PostMapping()
